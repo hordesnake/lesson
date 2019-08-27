f7a222ee46bb63f8ee8fdab055ef2128439a2df8
hordesnake <dlyakovl@gmail.com>
2019-08-27 16:49:00 +0300

    #

---
+++ prototype/operators.js
@@ -0,0 +1,35 @@
+//console.log( null || 2 || undefined );
+// console.log( alert(1) || 2 || alert(3) );
+// alert( 1 && null && 2 );
+// console.log( console.log(1) && console.log(2) ); //alert не возвращает значение
+// alert( null || 2 && 3 || 4 );
+// alert( 2 && 3 );
+// var age = prompt("Enter age");
+// console.log(age >= 14 && age <= 90);
+// console.log( -1 && 0) && alert( 'второе' );
+
+console.log( " " );
+console.log( "" - 1 + 0 );
+console.log( true + false );
+console.log( 6 / "3" );
+console.log( +"\n0\n" );
+// "2" * 4 + 5 "$" +
+// "4" ‐ "4px" 7/0
+// "3"
+// + "px" 4 + 5
+// 2
+// ‐ 2
+// " ‐9\n"+5 " ‐9\n"‐5 5 && 2
+// 2 && 5
+// 5 || 0
+// 0 || 5
+// null + 1 undefined + 1 null == "\n0\n" +null == +"\n0\n"
+
+
+// } else {
+// if = i (‐1 && 0)
+// console.log( 'второе' );
+// } else {
+// (null || ‐1 && 1)
+// console.log( 'третье' );
+// };
